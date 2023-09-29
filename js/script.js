3e8b8441b70b7d191cbf43b1ebdb74ce1640dfe9
ar78ilnik <ar78ilnik@gmail.com>
2023-09-29 16:31:00 +0300

    Создаем модальное окно и работаем с this

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // Timer
 
-    let deadline = '2023-09-28';
+    let deadline = '2023-09-30';
 
     // Узнаем сколько времени между сейчас и deadline
 
@@ -75,5 +75,23 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     setClock('timer', deadline);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 
 });
